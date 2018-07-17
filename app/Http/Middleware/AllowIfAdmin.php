a8b51955fcf04ba6368e54a5f1f5d479f16b0fde
nhatan172 <nhatan172@gmail.com>
2018-07-17 17:55:12 +0700

    add admin login

---
+++ app/Http/Middleware/AllowIfAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Auth;
+use Closure;
+
+class AllowIfAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->isAdmin())
+        {
+            return $next($request);
+        }
+        dd('Wrong Accept Header');
+    }
+}
